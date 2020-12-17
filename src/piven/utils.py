00b2d482616beab4f640556148858d6062233b38
Jasper Ginn <jasperginn@gmail.com>
2020-12-17 23:03:46 +0100

    feat: finish load/save models

--- src/piven/utils.py
+++ src/piven/utils.py
@@ -1,15 +1,16 @@
-# from pathlib import Path
-# import joblib
-from typing import Union
+import json
+from pathlib import Path
+import joblib
+from typing import Union, Callable
 import tensorflow as tf
 from piven.loss import piven_loss
 from piven.metrics import picp, mpiw
 from piven.layers import Piven
 from piven.wrappers import PivenModelWrapper
 from piven.transformers import PivenTransformedTargetRegressor
-from tensorflow.python.keras.engine.functional import Functional
-
-# from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.base import clone
 
 # Dump custom metrics, loss and layers
 # Need to do this when saving models.
@@ -18,19 +19,146 @@ tf.keras.utils.get_custom_objects().update(
 )
 
 
-def save_piven_model(
-    model: Union[PivenTransformedTargetRegressor, PivenModelWrapper, Functional] = None,
-    path: str = None,
+def _save_piven_model_wrapper(
+    model: PivenModelWrapper, path: Path, file_name: str = None
 ):
-    # Record type and store in config
-    # Check path is valid
-    # Store:
-    #  1. Keras model
-    #  2. PivenModelWrapper --> params
-    #  3. TransformedTargetRegressor --> Save Scaler
-    #  4. Pipeline --> save preprocessing steps
-    pass
+    """Save a keras model that is wrapped in a PivenModelWrapper class"""
+    if file_name is None:
+        file_name = "piven_model.h5"
+    tf.keras.models.save_model(model.model, path / file_name)
+    with (path / "piven_model_config.json").open("w") as outfile:
+        json.dump(model.sk_params, outfile)
+    return model
 
 
-def load_piven_model(build_fn=None, path: str = None):
-    pass
+def _unset_sklearn_pipeline(obj: Pipeline, path: Path):
+    """Unset a piven model from an sklearn pipeline and return the result"""
+    for idx, step in enumerate(obj.steps):
+        if isinstance(step[-1], PivenModelWrapper):
+            _ = _save_piven_model_wrapper(step[-1], path=path)
+            obj.steps[idx] = (step[0], None)
+    return obj
+
+
+def _save_model_pipeline(
+    obj: Union[Pipeline, StandardScaler], path: Path, file_name: str
+):
+    """Save an sklearn pipeline"""
+    # If piven model in pipeline
+    if isinstance(obj, Pipeline):
+        obj = _unset_sklearn_pipeline(obj, path=path)
+    joblib.dump(obj, path / file_name)
+    return obj
+
+
+def _save_piven_transformed_target_regressor(
+    regressor: PivenTransformedTargetRegressor, path: Path, file_name: str
+):
+    """Save a piven transformed target regressor"""
+    regressor.regressor = None
+    regressor.regressor_ = _unset_sklearn_pipeline(regressor.regressor_, path=path)
+    joblib.dump(regressor, path / file_name)
+    return regressor
+
+
+def _load_piven_model_wrapper(
+    path: Path, build_fn: Callable, model_config: dict
+) -> PivenModelWrapper:
+    """Load a keras model from disk and return a Piven wrapper"""
+    model = tf.keras.models.load_model(path / "piven_model.h5")
+    pmw = PivenModelWrapper(build_fn, **model_config)
+    pmw.model = model
+    return pmw
+
+
+def _load_model_config(path: Path) -> dict:
+    """Load PivenModelWrapper model config"""
+    with (path / "piven_model_config.json").open("r") as infile:
+        return json.load(infile)
+
+
+def _load_sklearn_pipeline(path: Path, build_fn: Callable) -> Pipeline:
+    """Load a sklearn pipeline from disk"""
+    pipeline = joblib.load(path / "piven_pipeline.joblib")
+    model = _load_piven_model_wrapper(
+        path, build_fn=build_fn, model_config=_load_model_config(path)
+    )
+    for idx, step in enumerate(pipeline.steps):
+        if step[-1] is None:
+            pipeline.steps[idx] = (step[0], model)
+    return pipeline
+
+
+def _load_piven_transformed_target_regressor(
+    path: Path, build_fn: Callable
+) -> PivenTransformedTargetRegressor:
+    """Load a transformed target regressor from disk"""
+    ttr = joblib.load(path / "piven_ttr.joblib")
+    ttr.regressor = clone(ttr.regressor_)
+    model_config = _load_model_config(path)
+    pmw = _load_piven_model_wrapper(path, build_fn=build_fn, model_config=model_config)
+    pmw_clone = PivenModelWrapper(build_fn, **model_config)
+    for idx, step in enumerate(ttr.regressor_.steps):
+        if step[-1] is None:
+            ttr.regressor_.steps[idx] = (step[0], pmw)
+            ttr.regressor.steps[idx] = (step[0], pmw_clone)
+    return ttr
+
+
+def save_piven_model(
+    model: Union[PivenTransformedTargetRegressor, PivenModelWrapper, Pipeline],
+    path: str,
+) -> Path:
+    """Save a piven model to a folder"""
+    ppath = Path(path)
+    if not ppath.is_dir():
+        raise NotADirectoryError(f"Directory {path} does not exist.")
+    if not isinstance(
+        model, (PivenTransformedTargetRegressor, PivenModelWrapper, Pipeline)
+    ):
+        raise TypeError(
+            "Model must be of type 'Pipeline', 'PivenModelWrapper'"
+            + " or 'PivenTransformedTargetRegressor'"
+        )
+    # Model config
+    config = {"type": str(type(model))}
+    # Save config
+    with (ppath / "config.json").open("w") as outfile:
+        json.dump(config, outfile)
+    # Dump model
+    if isinstance(model, PivenTransformedTargetRegressor):
+        _save_piven_transformed_target_regressor(model, ppath, "piven_ttr.joblib")
+    elif isinstance(model, Pipeline):
+        _save_model_pipeline(model, ppath, "piven_pipeline.joblib")
+    elif isinstance(model, PivenModelWrapper):
+        _save_piven_model_wrapper(model, ppath, "piven_model.h5")
+    else:
+        raise ValueError(
+            "Model must be of type 'Pipeline', 'PivenModelWrapper'"
+            + " or 'PivenTransformedTargetRegressor'"
+        )
+    # Return path
+    return path
+
+
+def load_piven_model(
+    build_fn: Callable, path: str
+) -> Union[PivenTransformedTargetRegressor, PivenModelWrapper, Pipeline]:
+    """Load a piven model from disk"""
+    ppath = Path(path)
+    if not ppath.is_dir():
+        raise NotADirectoryError(f"Directory {path} does not exist.")
+    with (ppath / "config.json").open("r") as infile:
+        config = json.load(infile)
+        mtype = config.get("type").strip("'>")
+    if mtype.endswith("PivenTransformedTargetRegressor"):
+        m = _load_piven_transformed_target_regressor(ppath, build_fn=build_fn)
+    elif mtype.endswith("Pipeline"):
+        m = _load_sklearn_pipeline(ppath, build_fn=build_fn)
+    elif mtype.endswith("PivenModelWrapper"):
+        m = _load_piven_model_wrapper(
+            ppath, build_fn=build_fn, model_config=_load_model_config(ppath)
+        )
+    else:
+        raise TypeError(f"Don't know how to load {mtype} from disk")
+    return m
