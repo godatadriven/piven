f07dcb5ff0ab0230f096a0232c5390e9946bb9a3
Jasper Ginn <jasperginn@gmail.com>
2020-12-17 18:51:22 +0100

    tests: add pytest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,18 @@
+from typing import Union
 import pytest
 import numpy as np
+from piven.wrappers import PivenModelWrapper
+from piven.transformers import PivenTransformedTargetRegressor
+from piven.metrics import picp, mpiw
+from piven.layers import Piven
+from piven.loss import piven_loss
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
 
 
 @pytest.fixture(scope="module")
-def mock_data():
+def mock_data() -> Union[np.array, np.array, np.array, np.array]:
     # fix seed
     seed = 26783
     np.random.seed(seed)
@@ -17,6 +26,42 @@ def mock_data():
     y_train = y[:400]
     x_valid = x[400:, :].reshape(-1, 1)
     y_valid = y[400:]
-    y_train = np.stack((y_train, y_train), axis=1)  # make this 2d so will be accepted
-    y_valid = np.stack((y_valid, y_valid), axis=1)
     return x_train, x_valid, y_train, y_valid
+
+
+@pytest.fixture(scope="function")
+def keras_model_function() -> tf.python.keras.engine.functional.Functional:
+    def keras_model(input_size, dropout_rate):
+        i = tf.keras.layers.Input(shape=(input_size,))
+        x = tf.keras.layers.Dense(128)(i)
+        x = tf.keras.layers.Dense(64)(x)
+        x = tf.keras.layers.Dropout(dropout_rate)(x)
+        o = Piven()(x)
+        m = tf.keras.models.Model(inputs=i, outputs=[o])
+        m.compile(
+            optimizer="adam",
+            loss=piven_loss(True, 15.0, 160.0, 0.05),
+            metrics=[picp, mpiw],
+        )
+        return m
+
+    return keras_model
+
+
+@pytest.fixture(scope="function")
+def piven_model_wrapper(keras_model_function) -> PivenModelWrapper:
+    return PivenModelWrapper(build_fn=keras_model_function(1, 0.1))
+
+
+@pytest.fixture(scope="function")
+def piven_model_pipeline(piven_model_wrapper) -> Pipeline:
+    return Pipeline([("scaler", StandardScaler()), ("model", piven_model_wrapper)])
+
+
+@pytest.fixture(scope="function")
+def transformed_piven_regressor(
+    piven_model_pipeline
+) -> PivenTransformedTargetRegressor:
+    return PivenTransformedTargetRegressor(
+        regressor=piven_model_pipeline, transformer=StandardScaler()
+    )
