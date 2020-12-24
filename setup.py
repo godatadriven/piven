4c97166e45e3241bc2f4c9540c6515d309bfc084
Jasper Ginn <jasperginn@gmail.com>
2020-12-24 16:38:32 +0100

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="piven",
-    version="0.2.1",
+    version="0.2.2",
     description="Prediction Intervals with specific value prediction",
     url="https://gitlab.com/jasperginn/piven.py",
     author="Jasper Ginn",
