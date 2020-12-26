6772464fdf69e9644383591d7275c0d4f3d03035
Jasper Ginn <jasperginn@gmail.com>
2020-12-26 19:31:04 +0100

    test: add tests for tf metrics

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -2,6 +2,7 @@ import pytest
 import numpy as np
 import tensorflow as tf
 from piven.metrics.numpy import piven_loss, coverage, pi_width
+from piven.metrics.tensorflow import picp, mpiw
 from piven.loss import piven_loss as piven_loss_tf
 
 
@@ -24,7 +25,7 @@ def mock_data(request):
 
 @pytest.mark.usefixtures("mock_data")
 class TestMetrics:
-    def test_piven_loss(self):
+    def test_np_piven_loss(self):
         loss = piven_loss(
             self.y_true, self.y_pred, self.y_pi_low, self.y_pi_high, 15.0, 160.0, 0.05
         )
@@ -51,10 +52,46 @@ class TestMetrics:
         loss_tf = piven_loss_tf(15.0, 160.0, 0.05)(y_true_prep, y_pred_prep).numpy()
         np.testing.assert_almost_equal(loss_np, loss_tf, decimal=4)
 
-    def test_coverage(self):
+    def test_np_coverage(self):
         cov = coverage(self.y_true, self.y_pi_low, self.y_pi_high)
         assert cov == 0.84
 
-    def test_pi_width(self):
+    def test_np_pi_width(self):
         piw = pi_width(self.y_pi_low, self.y_pi_high)
         assert np.round(piw, 3) == 0.943
+
+    def test_tf_picp(self):
+        y_pred_prep = tf.convert_to_tensor(
+            np.concatenate(
+                [self.y_pi_high.copy(), self.y_pi_low.copy(), self.y_weight.copy()],
+                axis=1,
+            ),
+            dtype=tf.float32,
+        )
+        y_true_prep = tf.convert_to_tensor(
+            np.concatenate(
+                [self.y_true.copy().reshape(-1, 1), self.y_true.copy().reshape(-1, 1)],
+                axis=1,
+            ),
+            dtype=tf.float32,
+        )
+        picp_out = picp(y_true_prep, y_pred_prep).numpy()
+        np.testing.assert_almost_equal(picp_out, 0.84, 2)
+
+    def test_tf_mpiw(self):
+        y_pred_prep = tf.convert_to_tensor(
+            np.concatenate(
+                [self.y_pi_high.copy(), self.y_pi_low.copy(), self.y_weight.copy()],
+                axis=1,
+            ),
+            dtype=tf.float32,
+        )
+        y_true_prep = tf.convert_to_tensor(
+            np.concatenate(
+                [self.y_true.copy().reshape(-1, 1), self.y_true.copy().reshape(-1, 1)],
+                axis=1,
+            ),
+            dtype=tf.float32,
+        )
+        mpiw_out = mpiw(y_true_prep, y_pred_prep).numpy()
+        np.testing.assert_almost_equal(mpiw_out, 0.943, 3)
