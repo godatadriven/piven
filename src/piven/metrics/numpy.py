ae9d99506c84b9c498ab15b97a9a34026ce4d3f0
Jasper Ginn <jasperginn@gmail.com>
2020-12-27 12:27:22 +0100

    fix: numpy metrics now return floats, not np arrays

--- src/piven/metrics/numpy.py
+++ src/piven/metrics/numpy.py
@@ -3,18 +3,16 @@ from sklearn.metrics import mean_squared_error
 from scipy.special import expit
 
 
-def coverage(
-    y_true: np.ndarray, pi_lower: np.ndarray, pi_higher: np.ndarray
-) -> np.ndarray:
+def coverage(y_true: np.ndarray, pi_lower: np.ndarray, pi_higher: np.ndarray) -> float:
     """Compute the coverage of a PIVEN estimator"""
     covered_lower = pi_lower < y_true
     covered_higher = pi_higher > y_true
-    return np.mean(covered_lower * covered_higher)
+    return float(np.mean(covered_lower * covered_higher))
 
 
-def pi_width(pi_lower: np.ndarray, pi_higher: np.ndarray) -> np.ndarray:
+def pi_width(pi_lower: np.ndarray, pi_higher: np.ndarray) -> float:
     """Compute the average distance between the upper and lower bounds"""
-    return np.mean(pi_higher - pi_lower)
+    return float(np.mean(pi_higher - pi_lower))
 
 
 def piven_loss(
