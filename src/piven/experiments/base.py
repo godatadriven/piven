c2094eea5172038b20dcac55696b7002c1e5d0db
Jasper Ginn <jasperginn@gmail.com>
2020-12-28 10:00:59 +0100

    refactor: cleaned up code and scoring is no longer abstract method

--- src/piven/experiments/base.py
+++ src/piven/experiments/base.py
@@ -4,11 +4,13 @@ from pathlib import Path
 from typing import Callable, Dict
 import pandas as pd
 import numpy as np
+from sklearn.metrics import mean_absolute_error as mae, mean_squared_error as mse
 from sklearn.exceptions import NotFittedError
+from piven.metrics.numpy import coverage, pi_width, piven_loss as piven_loss_numpy
 from piven.utils import save_piven_model, load_piven_model
 
 
-class PivenExperiment(metaclass=abc.ABCMeta):
+class PivenBaseExperiment(metaclass=abc.ABCMeta):
     def __init__(self, **model_params):
         self.params = model_params
         self.model = None
@@ -21,6 +23,7 @@ class PivenExperiment(metaclass=abc.ABCMeta):
 
     def fit(self, x: np.ndarray, y: np.ndarray, **kwargs):
         self.model.fit(x, y, **kwargs)
+        return self
 
     def predict(self, x: np.ndarray, return_prediction_intervals: bool = True):
         if self.model is None:
@@ -30,8 +33,7 @@ class PivenExperiment(metaclass=abc.ABCMeta):
     def save(self, path: str):
         if self.model is None:
             raise NotFittedError("Model has not yet been fitted.")
-        ppath = Path(path)
-        with (ppath / "experiment_params.json").open("w") as outfile:
+        with (Path(path) / "experiment_params.json").open("w") as outfile:
             json.dump(self.params, outfile)
         save_piven_model(self.model, path)
         return self
@@ -59,19 +61,41 @@ class PivenExperiment(metaclass=abc.ABCMeta):
             mpath.mkdir()
             self.save(str(mpath))
         if predictions:
-            pred_out = np.column_stack([y, y_pred, y_pi_low, y_pi_high])
             df = pd.DataFrame(
-                data=pred_out, columns=["y_true", "y_pred", "y_pi_low", "y_pi_high"]
+                data=np.column_stack([y, y_pred, y_pi_low, y_pi_high]),
+                columns=["y_true", "y_pred", "y_pi_low", "y_pi_high"],
             )
             df.to_csv((ppath / "predictions.csv"))
         return self
 
+    def score(
+        self,
+        y_true: np.ndarray,
+        y_pred: np.ndarray,
+        y_pi_low: np.ndarray,
+        y_pi_high: np.ndarray,
+    ) -> Dict:
+        return {
+            "loss": piven_loss_numpy(
+                y_true,
+                y_pred,
+                y_pi_low,
+                y_pi_high,
+                self.params.get("lambda_"),
+                160.0,
+                0.05,
+            ),
+            "mae": mae(y_true, y_pred),
+            "rmse": np.sqrt(mse(y_true, y_pred)),
+            "coverage": coverage(y_true, y_pi_low, y_pi_high),
+            "pi_width": pi_width(y_pi_low, y_pi_high),
+        }
+
     @staticmethod
     def load_experiment_config(path: str):
-        ppath = Path(path)
-        if not (ppath / "experiment_params.json").is_file():
+        if not (Path(path) / "experiment_params.json").is_file():
             raise FileNotFoundError(f"No experiment file found in {path}.")
-        with (ppath / "experiment_params.json").open("r") as infile:
+        with (Path(path) / "experiment_params.json").open("r") as infile:
             params = json.load(infile)
         return params
 
@@ -86,14 +110,4 @@ class PivenExperiment(metaclass=abc.ABCMeta):
     @classmethod
     @abc.abstractmethod
     def load(cls, path: str):
-        pass
-
-    @abc.abstractmethod
-    def score(
-        self,
-        y_true: np.ndarray,
-        y_pred: np.ndarray,
-        y_pi_low: np.ndarray,
-        y_pi_high: np.ndarray,
-    ) -> Dict:
         pass
